495e724364fee616b126c69c299ad6ee0b0e1902
Michał <mich.gor@wp.pl>
2021-12-01 21:49:13 +0100

    Added first migration.

---
+++ EzLearning.Server/EzLearning.Server.Dal/Migrations/20211201200433_FirstMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EzLearning.Server.Dal.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Username = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PasswordHash = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedTimestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ EzLearning.Server/EzLearning.Server.Dal/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EzLearning.Server.Dal.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public DateTime UpdatedTimestamp { get; set; }
+    }
+}
