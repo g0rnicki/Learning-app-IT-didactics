9a9b4685273af8237af70b82e457b6de697aefd0
g0rnicki <to22masz@gmail.com>
2022-02-05 19:58:04 +0200

    small changes_2

---
+++ Test_app_1/Test_app_1/Views/LessonsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test_app_1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LessonsPage : ContentPage
+    {
+        public LessonsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"{nameof(LessonsPage)}");
+        }
+    }
+}
