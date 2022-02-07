5592595fe4e7425daa7bcca47dc61b9a2bce01d5
Michał <mich.gor@wp.pl>
2022-02-07 20:30:14 +0100

    Shell for more chapters data seed migration

---
+++ EzLearning.Server/EzLearning.Server.Dal/Migrations/20220207192729_SeedMoreChaptersData.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EzLearning.Server.Dal.Migrations
+{
+    public partial class SeedMoreChaptersData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "chapters",
+                columns: new[] { "Name" },
+                values: new object[] { "" });
+
+            migrationBuilder.InsertData(
+                table: "chapters",
+                columns: new[] { "Name" },
+                values: new object[] { "" });
+
+            migrationBuilder.InsertData(
+                table: "chapters",
+                columns: new[] { "Name" },
+                values: new object[] { "" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
