6ae34de12842f4ab566400d932ac4ccc1cd78449
Michał <mich.gor@wp.pl>
2022-02-09 02:26:11 +0100

    Return UserId when login

--- EzLearning.Server/EzLearning.Server.Services/Contracts/LoginResult.cs
+++ EzLearning.Server/EzLearning.Server.Services/Contracts/LoginResult.cs
@@ -1,8 +1,11 @@
-﻿namespace EzLearning.Server.Services.Contracts
+﻿using System;
+
+namespace EzLearning.Server.Services.Contracts
 {
     public class LoginResult
     {
         public bool IsSuccessfull { get; set; }
         public string Token { get; set; }
+        public Guid UserId { get; set; }
     }
 }
