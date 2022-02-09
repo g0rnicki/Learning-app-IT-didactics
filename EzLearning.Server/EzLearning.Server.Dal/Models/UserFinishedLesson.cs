63924f541a0124deb818056cc0026118e8a64169
Michał <mich.gor@wp.pl>
2022-02-09 01:53:23 +0100

    Migration with progress table

---
+++ EzLearning.Server/EzLearning.Server.Dal/Models/UserFinishedLesson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EzLearning.Server.Dal.Models
+{
+    public class UserFinishedLesson
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public int LessonNumber { get; set; }
+    }
+}
